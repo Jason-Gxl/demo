90f2a7079cf4906f0350322243f66c1a408bdf15
Jason-Gxl <2796761@qq.com>
2015-10-15 18:48:50 +0800

    update

--- fixedBox/fixedBox.js
+++ fixedBox/fixedBox.js
@@ -6,11 +6,8 @@
 
 		var _fb = function(options) {
 			this.obj = options.obj;
-			var objOpt = options.obj.getBoundingClientRect();
-			this.initOpt = {
-				"x": options.obj.offsetLeft,
-				"y": options.obj.offsetTop
-			};
+			this.initOpt = options.obj.getBoundingClientRect();
+			this.initOpt.oldStyle = this.obj.getAttribute("style");
 			this.wheelEvent();
 		};
 
@@ -22,10 +19,14 @@
 				var so = function() {
 					var e = arguments[0] || window.event;
 					var _bs = self.BS();
-					if(_bs.top>=self.initOpt.y) {
-						if(self.obj.className.indexOf("fixedBox")==-1) self.obj.className = self.obj.className.replace(/(^\s*)|(\s*$)/g, "") +  " fixedBox";
+					if(_bs.top>=self.initOpt.top) {
+						if(self.obj.className.indexOf("fixedBox")==-1) {
+							self.obj.style.left = self.initOpt.left - 20 + "px";
+							self.obj.className = self.obj.className.replace(/(^\s*)|(\s*$)/g, "") +  " fixedBox";
+						}
 					} else {
 						self.obj.className = self.obj.className.replace("fixedBox", "").replace(/(^\s*)|(\s*$)/g, "");
+						if(self.initOpt.oldStyle) self.obj.setAttribute("style", self.initOpt.oldStyle);
 					}
 				};
 
@@ -40,7 +41,7 @@
 					dd=document.documentElement,
 					top = db.scrollTop+dd.scrollTop;
 					left = db.scrollLeft+dd.scrollLeft;
-				return { 'top':top , 'left':left };
+				return {'top':top , 'left':left};
 			}
 		};
 
